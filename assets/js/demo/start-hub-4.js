428ecef0065617757cbed8d7514d01993dee942b
anwholesquare <anwholesquare@gmail.com>
2023-10-25 06:51:52 +0600

    Bizstori latest website

--- assets/js/demo/start-hub-4.js
+++ assets/js/demo/start-hub-4.js
@@ -1,6 +1,6 @@
-( () => {
-	const wrapper_093c16a = document.getElementById( 'lqd-lottie-confetti' );
-	const animItem_093c16a = bodymovin.loadAnimation( {
+(() => {
+	const wrapper_093c16a = document.getElementById('lqd-lottie-confetti');
+	const animItem_093c16a = bodymovin.loadAnimation({
 		wrapper: wrapper_093c16a,
 		animType: 'canvas',
 		name: 'lqd-lottie-confetti',
@@ -10,25 +10,26 @@
 		rendererSettings: {
 			className: 'lqd-lottie',
 		}
-	} );
+	});
 
-	lottie.setDirection( 1, 'lqd-lottie-confetti' );
-	lottie.setSpeed( 0.7, 'lqd-lottie-confetti' );
+	lottie.setDirection(1, 'lqd-lottie-confetti');
+	lottie.setSpeed(0.7, 'lqd-lottie-confetti');
 
-	lottie.pause( 'lqd-lottie-confetti' );
+	lottie.pause('lqd-lottie-confetti');
 
-	new IntersectionObserver( ( [ entry ] ) => {
-		if ( entry.isIntersecting ) {
-			lottie.play( 'lqd-lottie-confetti' );
-		} else {
-			lottie.pause( 'lqd-lottie-confetti' )
-		}
-	} ).observe( wrapper_093c16a )
-
+	try {
+		new IntersectionObserver(([entry]) => {
+			if (entry.isIntersecting) {
+				lottie.play('lqd-lottie-confetti');
+			} else {
+				lottie.pause('lqd-lottie-confetti')
+			}
+		}).observe(wrapper_093c16a)
+	} catch (e) { }
 	// ******
 
-	const wrapper_c833602 = document.getElementById( 'lqd-lottie-footer' );
-	const animItem_c833602 = bodymovin.loadAnimation( {
+	const wrapper_c833602 = document.getElementById('lqd-lottie-footer');
+	const animItem_c833602 = bodymovin.loadAnimation({
 		wrapper: wrapper_c833602,
 		animType: 'svg',
 		name: 'lqd-lottie-footer',
@@ -38,18 +39,18 @@
 		rendererSettings: {
 			className: 'lqd-lottie',
 		}
-	} );
+	});
 
-	lottie.setDirection( 1, 'lqd-lottie-footer' );
-	lottie.setSpeed( 0.5, 'lqd-lottie-footer' );
+	lottie.setDirection(1, 'lqd-lottie-footer');
+	lottie.setSpeed(0.5, 'lqd-lottie-footer');
 
-	lottie.pause( 'lqd-lottie-footer' );
+	lottie.pause('lqd-lottie-footer');
 
-	new IntersectionObserver( ( [ entry ] ) => {
-		if ( entry.isIntersecting ) {
-			lottie.play( 'lqd-lottie-footer' );
+	new IntersectionObserver(([entry]) => {
+		if (entry.isIntersecting) {
+			lottie.play('lqd-lottie-footer');
 		} else {
-			lottie.pause( 'lqd-lottie-footer' )
+			lottie.pause('lqd-lottie-footer')
 		}
-	} ).observe( wrapper_c833602 )
-} )();
+	}).observe(wrapper_c833602)
+})();
--- script.js
+++ script.js
@@ -3,6 +3,16 @@ document.getElementById("openModalBtn").addEventListener("click", function () {
     showStep(1);
 });
 
+document.getElementById("openModalBtn1").addEventListener("click", function () {
+    document.getElementById("myModal").style.display = "block";
+    showStep(1);
+});
+
+document.getElementById("openModalBtn2").addEventListener("click", function () {
+    document.getElementById("myModal").style.display = "block";
+    showStep(1);
+});
+
 document.getElementById("closeModalBtn").addEventListener("click", function () {
     document.getElementById("myModal").style.display = "none";
 });
@@ -16,26 +26,60 @@ function showStep(stepNumber) {
 }
 
 document.getElementById("nextStepBtn1").addEventListener("click", function () {
-    showStep(2);
+    let canProceed = false;
+    for (let i = 0; i < selectedServices.length; i++) {
+        if (selectedServices[i] == true) {
+            canProceed = true;
+            break;
+        }
+    }
+    if (canProceed) {
+        showStep(2);
+    } else {
+        alert("Please select at least one service");
+    }
 });
 
 document.getElementById("nextStepBtn2").addEventListener("click", function () {
     showStep(3);
 });
 
+
 document.getElementById("nextStepBtn3").addEventListener("click", function () {
-    showStep(4);
+    if (document.getElementById("mobile").value.length > 7) {
+        showStep(4);
+    } else {
+        alert("Please enter a valid mobile number");
+    }
 });
+
+document.getElementById("nextStepBtn4").addEventListener("click", function () {
+    document.getElementById("myModal").style.display = "none";
+});
+
+
+function isValidEmail(email) {
+    // Regular expression for a valid email address
+    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(email);
+}
 
 
 
 document.getElementById("submitBtn").addEventListener("click", function () {
-    postData();
+    if (document.getElementById("fullname").value.length < 3 || !isValidEmail(document.getElementById("email").value)) {
+        alert("Please enter valid details");
+    } else {
+        postData();
+        showStep(5);
+    }
+
 });
 
 // You can add validation and form submission logic for step 3 (personal details) here
 
-var selectedServices = [false, false, false, false, false, false];
+var selectedServices = [false, false, false, false, false, false, false];
+var selectedServicesName = ["Website Design & Development", "App Design & Development", "Content Creation & SEO", "Social Media Marketing", "Pay-Per-Click (PPC) Advertising", "Online Reputation Management & Email Marketing", "Tele Marketing & Support"];
 var budget = 0;
 
 function chooseService(index) {
@@ -95,27 +139,63 @@ rangeInput.forEach((input) => {
 
 function postData() {
 
+    let ServiceString = "";
+    for (let i = 0; i < selectedServices.length; i++) {
+        if (selectedServices[i] == true) {
+            ServiceString += (selectedServicesName[i] + ", ");
+        }
+    }
+
+    let minPrice = parseInt(priceInput[0].value);
+    let maxPrice = parseInt(priceInput[1].value);
+    let budget = "$" + minPrice + " - $" + maxPrice;
+
+
     var data = {
         fullname: document.getElementById("fullname").value,
-        services: "Web Development",
-        estimatedBudget: "$5000",
+        services: ServiceString,
+        estimatedBudget: budget,
         phoneNumber: document.getElementById("mobile").value,
         emailAddress: document.getElementById("email").value
     };
-    fetch('https://script.google.com/macros/s/AKfycbyxaxcu05wDi_wKnibk9u0W6p4iHTcKwZhgmLZ5Xo8WmceUSFCEPKhx79MDik4N0CUt/exec', {
-        redirect: "follow",
+
+    fetch('./storeData.php', {
         method: 'POST',
-        body: JSON.stringify(data),
         headers: {
-            "Content-Type": "text/plain;charset=utf-8"
-        }
+            'Content-Type': 'application/x-www-form-urlencoded', // Adjust the content type as needed
+        },
+        body: new URLSearchParams(data).toString(),
     })
-        .then(response => alert(response.text()))
-        .then(text => console.log(text))
-        .catch(error => console.log('Error:', error));
+        .then(response => {
+            // Handle the response from PHP if necessary
+            console.log('Request to PHP script was successful');
+        })
+        .catch(error => console.error('Error:', error));
+
+    // fetch('./storeData.php', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/x-www-form-urlencoded', // Adjust the content type as needed
+    //     },
+    //     body: new URLSearchParams(data).toString(),
+    // })
+    //     .then(response => {
+    //         // Handle the response from PHP if necessary
+    //         console.log('Request to PHP script was successful');
+    //     })
+    //     .catch(error => console.error('Error:', error));
 
 
 }
+
+
+
+
+
+
+
+
+
 
 
 
